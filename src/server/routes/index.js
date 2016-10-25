64475b33826a10ecb105dd7bdbd3b0571fe743c6
Clemich38 <clement.letreguilly@gmail.com>
2016-10-25 16:59:18 +0900

    unusful code removed

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -2,24 +2,11 @@ const express = require('express');
 var path = require('path');
 const router = express.Router();
 
-// const indexController = require('../controllers/index');
 
 // GET home page
 router.get('/', (req, res, next) => {
   res.sendFile(path.join(
     __dirname, '..', '..', 'client', 'index.html'));
 });
-
-// router.get('/', function (req, res, next) {
-//   const renderObject = {};
-//   renderObject.title = 'Welcome to Express!';
-//   indexController.sum(1, 2, (error, results) => {
-//     if (error) return next(error);
-//     if (results) {
-//       renderObject.sum = results;
-//       res.render('index', renderObject);
-//     }
-//   });
-// });
 
 module.exports = router;
